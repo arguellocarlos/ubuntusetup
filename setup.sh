7d9d4223290bc499b0ab28ff6b91c293cedfc57f
Carlos Arguello <84431643+arguellocarlos@users.noreply.github.com>
2024-10-29 02:12:21 -0500

    Update Firefox installation again

--- setup.sh
+++ setup.sh
@@ -86,14 +86,14 @@ install_firefox(){
     sudo install -d -m 0755 /etc/apt/keyrings
     wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
     echo ""deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main"" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
-    echo "echo '
-    Package: *
-    Pin: origin packages.mozilla.org
-    Pin-Priority: 1000
+    echo "
+Package: *
+Pin: origin packages.mozilla.org
+Pin-Priority: 1000
 
-    Package: firefox*
-    Pin: release o=Ubuntu
-    Pin-Priority: -1'" | sudo tee /etc/apt/preferences.d/mozilla
+Package: firefox*
+Pin: release o=Ubuntu
+Pin-Priority: -1" | sudo tee /etc/apt/preferences.d/mozilla
     
     sudo apt update -y
     sudo apt install firefox -y
