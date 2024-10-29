5d5e4c4fffbb80f06574efd9c7c7b49809ffcc5a
Carlos Arguello <84431643+arguellocarlos@users.noreply.github.com>
2024-10-29 00:18:25 -0500

    Updated .md

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ install_brave() {
     echo "Installing Brave browser..."
     sudo apt install curl -y
     sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-    echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+    echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
     sudo apt update -y
     sudo apt install brave-browser -y
 }
--- subiquityinstallscript.md
+++ subiquityinstallscript.md
@@ -1,2 +1,5 @@
 [Source 1](https://github.com/canonical/subiquity/blob/main/doc/tutorial/providing-autoinstall.rst)
+
+***
+
 [Source 2](https://github.com/local-optimum/desktop-example-autoinstall/blob/main/autoinstall.yaml)
