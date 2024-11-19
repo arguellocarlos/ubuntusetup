7bbe9fda78b0b943fe0442bac3455b2b20adfd51
Carlos Arguello <84431643+arguellocarlos@users.noreply.github.com>
2024-11-18 20:00:20 -0600

    Removed *.py files - Moving to a different repo

--- packageinstall.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-def install_packages():
-    packages = ["curl", "build-essential", "wget"]
-    for package in packages:
-        subprocess.run(["sudo", "apt-get", "install", "-y", package])
-    print("Packages installed successfully.")
